1614a1def3c515eda20adee16aa07fa9bd3a2ca9
savasduzgun <savasdzgn@gmail.com>
2024-07-24 20:40:32 +0300

    Add project files.

---
+++ VideoTranscriberApp.ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Resources;
+using Xabe.FFmpeg;
+
+var videoFilePath = "C:\\Users\\user\\Desktop\\YAZILIM ACADEMY\\Sam-Bankman-Fried.mp4";
+
+string audioFilePath = Path.ChangeExtension(videoFilePath, "mp3");
+
+CancellationTokenSource cts = new CancellationTokenSource();
+
+var conversion = await FFmpeg.Conversions.FromSnippet.ExtractAudio(videoFilePath, audioFilePath);
+
+await conversion.Start(cts.Token);
